6cfee5230a7cf51f27ec274a62daecad441b4889
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-02-27 22:17:21 +0530

    Update start.py

--- DAXXMUSIC/utils/inline/start.py
+++ DAXXMUSIC/utils/inline/start.py
@@ -31,6 +31,9 @@ def private_panel(_):
         [
            
           InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
+            
+        ],
+        [ 
           InlineKeyboardButton(text=_["S_B_11"], url=config.SUPPORT_Channel),
     ]
     return buttons
