43631a6fa2a50a7b871f01059bf6ecbd3d4862ff
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-04-19 15:54:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/dattudd/Dfsxmusic",
+    "https://github.com/dattudd/shizuka-x-music-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
